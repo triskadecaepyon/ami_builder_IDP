b7ba4defb2b86a75169da564c95cda0b288d0ff5
triskadecaepyon <dcltechnology@gmail.com>
2018-09-09 13:21:32 -0500

    adding in initial Packer AMI files

---
+++ ubuntu_base/build_idp3_core.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+wget --quiet https://repo.anaconda.com/miniconda/Miniconda2-4.5.4-Linux-x86_64.sh -O ~/miniconda.sh && \
+    /bin/bash ~/miniconda.sh -b -p /home/ubuntu/conda && \
+    rm ~/miniconda.sh && \
+    sudo ln -s /home/ubuntu/conda/etc/profile.d/conda.sh /etc/profile.d/conda.sh && \
+    echo ". /home/ubuntu/conda/etc/profile.d/conda.sh" >> ~/.bashrc && \
+    echo "conda activate base" >> ~/.bashrc
+
+. /home/ubuntu/conda/etc/profile.d/conda.sh
+conda config --add channels intel \
+    && export ACCEPT_INTEL_PYTHON_EULA=yes \
+    && conda create -n idp intelpython3_core=2018.0.3 python=3 -y -q \
+    && conda clean --all -y \
+    && echo "conda activate idp" >> ~/.bashrc \
+    && sudo apt-get update -qqq \
+    && sudo apt-get install -y -q g++ \
+    && sudo apt-get autoremove
