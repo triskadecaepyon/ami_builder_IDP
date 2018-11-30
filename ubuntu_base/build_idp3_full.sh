c93ec7abcdd72b956d764791a549aa1c9b6597e7
triskadecaepyon <dcltechnology@gmail.com>
2018-11-30 09:43:25 -0600

    updating for 2019 release

--- ubuntu_base/build_idp3_full.sh
+++ ubuntu_base/build_idp3_full.sh
@@ -8,7 +8,7 @@ wget --quiet https://repo.anaconda.com/miniconda/Miniconda2-4.5.4-Linux-x86_64.s
 . /home/ubuntu/conda/etc/profile.d/conda.sh
 conda config --add channels intel \
     && export ACCEPT_INTEL_PYTHON_EULA=yes \
-    && conda create -n idp intelpython3_full=2018.0.3 python=3 -y -q \
+    && conda create -n idp intelpython3_full=2019.1 python=3 -y -q \
     && conda clean --all -y \
     && echo "conda activate idp" >> ~/.bashrc \
     && sudo apt-get update -qqq \
